a664b054d3d9f47d3c2ef7a7fab89d0535194c40
田中 一馬 <kazuma.sushisushi1506@ezweb.ne.jp>
2017-02-08 23:35:15 -0600

    yrdy

---
+++ public/scripts/nlc.js
@@ -0,0 +1,24 @@
+function NLCLists(){
+	// [id="target"]を取得 (<p id="target">目的の要素</p>)
+	/*var element = document.getElementById( "testData" ) ;
+	
+	// 取得した要素の文字色を変更する (応用)
+	element.style.color = "#D36015" ;*/
+	window.alert("test");
+  //テストデータ
+ /* natural_language_classifier.list({},
+  function(err, response) {
+      if (err)
+          console.log('error:', err);
+        else
+          //console.log(JSON.stringify(response, null, 2));
+          var classifiers = response.classifiers;
+          for (var i=0; i < classifiers.length; i++){
+            var classfiersBox = classifiers[i];
+            var classifier_id = classfiersBox.classifier_id;
+            var name = classfiersBox.name;
+            console.log('classifier_id: ' + classifier_id + '  name: '+ name);
+          }
+  });*/
+
+}
