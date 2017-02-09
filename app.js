c20e2db3dcafac2234ea572f4f046ef50c6da799
田中 一馬 <kazuma.sushisushi1506@ezweb.ne.jp>
2017-02-08 22:42:52 -0600

    watsonとの連携

--- app.js
+++ app.js
@@ -26,7 +26,47 @@ app.use(bodyParser.json());
 //Lineへ送る
 var request = require('request');
 
+//watson
+var watson = require('watson-developer-cloud');
+var username = '28aeb4cc-22d0-4261-a89c-bbc75fab34fb';
+var password = 'pPqgxASkaxVJ';
+var classifier_id = '';
+var natural_language_classifier = watson.natural_language_classifier({
+  username: username,
+  password: password,
+  version: 'v1'
+});
+
 app.post('/api', function(req, res) {
+	natural_language_classifier.list({},
+	  function(err, response) {
+	      if (err)
+	          console.log('error:', err);
+	        else
+	          //console.log(JSON.stringify(response, null, 2));
+	          var classifiers = response.classifiers;
+	          var options = {
+			    method: 'POST',
+			    uri: 'https://api.line.me/v2/bot/message/reply',
+			    body: {
+			      replyToken: req.body.events[0].replyToken,
+			      messages: [{
+			        type: "text",
+			        text: classifiers[0].name
+			      }]
+			    },
+			    auth: {
+			      bearer: '7l+bp0R0SNWVmWUD1QDCB6w0YoBjUmsTjp3zsJfl90Gl6uZZGgOw1VT25JfnPzz8xyhYYMZDmgsN8hd222qfd+QGVsZ5AsENs22VguuQBYpGuXgtHK6i413DyKT4tZxCRO3+ckrwXYWikNLRaNGPUQdB04t89/1O/w1cDnyilFU=' // ここは自分のtokenに書き換える
+			    },
+			    json: true
+			  };
+			  request(options, function(err, res, body) {
+			    console.log(JSON.stringify(res));
+			  });
+			  res.send('OK');
+  });  
+});
+/*app.post('/api', function(req, res) {
   var options = {
     method: 'POST',
     uri: 'https://api.line.me/v2/bot/message/reply',
@@ -46,10 +86,12 @@ app.post('/api', function(req, res) {
     console.log(JSON.stringify(res));
   });
   res.send('OK');
-});
+});*/
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
